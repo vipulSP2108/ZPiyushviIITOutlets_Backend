727d7805288828d8ca1f886c0a614139907468db
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2024-12-31 05:33:20 +0530

    Update UseDetails.js

--- UseDetails.js
+++ UseDetails.js
@@ -6,6 +6,9 @@ const userSchema = new mongoose.Schema({
     // contactinfo: { type: String, required: true, unique: false },
     password: { type: String, required: true },
     role: { type: String, required: true },
+    otp: { type: String },
+    otpExpiry: { type: Date },
+    isVerified: { type: Boolean, required: true },
 });
 // userSchema.index({ contactinfo: 1, role: 1 }, { unique: true });
 
